d7c18ceecd81869b6e9d3f0056cdf56767d0d194
jagadeeshgadi1997-cpu <jagadeeshgadi1997@gmail.com>
2026-04-16 11:06:59 +0530

    current error fixed

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -39,7 +39,7 @@ const LoginPage = () => {
         
         {/* Title */}
         <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
-         previous Login
+         current Login
         </h2>
       
 
