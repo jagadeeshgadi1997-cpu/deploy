d0390efe398521e10ae8a9c61cda21c9abaf1fa1
jagadeeshgadi1997-cpu <jagadeeshgadi1997@gmail.com>
2026-04-16 10:59:05 +0530

    fix error

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const LoginPage = () => {
-    const x=;
+    const x=10;
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -41,7 +41,7 @@ const LoginPage = () => {
         <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
           Login
         </h2>
-        {const x = ;}
+      
 
         {/* Message */}
         {message && (
